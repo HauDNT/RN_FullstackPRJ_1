00dcba9321987fe8d7000787f48c9cb44aa87a85
HauDNT <tienhau.it@gmail.com>
2024-07-19 14:49:01 +0700

    Add banner

---
+++ client/data/BannerData.js
@@ -0,0 +1,26 @@
+export const bannerData = [
+    {
+        _id: 1,
+        coverImageUri: 'https://img.freepik.com/premium-psd/horizontal-website-banne_451189-114.jpg',
+        cornerLabelColor: '#FFD300',
+        cornerLabelText: '-25%',
+    },
+    {
+        _id: 2,
+        coverImageUri: 'https://img.freepik.com/premium-psd/gaming-computer-sale-promotion-banner_252779-1160.jpg',
+        cornerLabelColor: '#0080ff',
+        cornerLabelText: 'NEW',
+    },
+    {
+        _id: 3,
+        coverImageUri: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/tablet-banner-sale-design-template-836a6a6b7e48e458c4837143e0674781_screen.jpg?ts=1649322328',
+        cornerLabelColor: '#2ECC40',
+        cornerLabelText: '-75%',
+    },
+    {
+        _id: 4,
+        coverImageUri: 'https://cdn.create.vista.com/downloads/8aad5a90-8526-4bf3-aee4-701af7d28293_1024.jpeg',
+        cornerLabelColor: '#2ECC40',
+        cornerLabelText: '-20%',
+    },
+];
--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,11 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
 import Layout from "../components/layouts/Layout";
 import Categories from "../components/category/Categories";
+import Banner from "../components/banner/Banner";
 
 const Home = () => {
     return (
         <Layout>
             <Categories/>
+            <Banner/>
             <View>
                 <Text>Home Page</Text>
             </View>
