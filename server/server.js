3728c3a834cf8ccaa2649837f238f9b8ed65d899
Thomas <tienhau.it@gmail.com>
2024-07-31 22:42:04 +0700

    Connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ import colors from "colors";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 // dot env config:
 dotenv.config();
+
+// Database connection:
+connectDB();
 
 // Rest object
 const app = express();
@@ -19,8 +23,6 @@ app.use(cors());
 app.get("/", (req, res) => {
     return res.status(200).send("<h1>Welcome to Server</h1>");
 });
-
-
 
 // Port:
 const PORT = process.env.PORT || 5000;
