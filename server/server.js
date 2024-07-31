fe91a3ee24344b5cf3f1e9729ea5d3e894d9fb96
Thomas <tienhau.it@gmail.com>
2024-07-31 21:36:36 +0700

    Init server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import colors from "colors";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// dot env config:
+dotenv.config();
+
+// Rest object
+const app = express();
+
+// Middlewares:
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+// Routes:
+app.get("/", (req, res) => {
+    return res.status(200).send("<h1>Welcome to Server</h1>");
+});
+
+
+
+// Port:
+const PORT = process.env.PORT || 5000;
+
+// Listen:
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`.blue);
+});
+
